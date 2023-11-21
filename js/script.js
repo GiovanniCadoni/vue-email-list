db2d6af8e27337977f2c0584117089e754903014
GiovanniCadoni <giocadoni19@gmail.com>
2023-11-21 18:47:14 +0100

    add: generate random email

--- js/script.js
+++ js/script.js
@@ -2,9 +2,20 @@ const { createApp } = Vue;
 
 createApp({
     data() {
-
+        return {
+            randomEmail: [],
+        };
     },
     methods: {
-
+        generateEmail() {
+            for(let i = 0; i < 10; i++)
+            {
+                axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp) => {
+                    this.randomEmail.push(resp.data.response);
+                });
+            }
+        },
     },
 }).mount("#app");
